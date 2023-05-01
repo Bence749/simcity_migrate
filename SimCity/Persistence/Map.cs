f72c74b49490c8cef3795d91f1dc708ba389c027
Bence Zsolt Beregi <dqk6te@INF.ELTE.HU>
2023-05-01 15:37:21 +0200

    SIM-43: Read for develop the audio feedback for the user for frontend.

--- SimCity/Persistence/Map.cs
+++ SimCity/Persistence/Map.cs
@@ -37,7 +37,7 @@ public class Map
             _fields[rows / 2 - 1, 0] = _fields[(rows / 2) + 1, 0] = new Road();
     }
 
-    public Int32 maxCitizens()
+    public Int32 MaxCitizens()
     {
         Int32 sumCit = 0;
 
@@ -48,7 +48,7 @@ public class Map
         return sumCit;
     }
     
-    public Int32 getMaintenance()
+    public Int32 GetMaintenance()
     {
         Int32 sumMaintenance = 0;
 
@@ -60,24 +60,42 @@ public class Map
         return sumMaintenance;
     }
     
+    /// <summary>
+    /// Set the <c>_fields</c> value at position <c>row, column</c> to the requested child of <see cref="AreaType" />
+    /// </summary>
+    /// <param name="row">Row of the desired element.</param>
+    /// <param name="column">Column of the desired element</param>
+    /// <param name="toBuild">Child of <see cref="AreaType" /> to be built.</param>
+    /// <returns><see cref="Int32" /> cost to be deducted from the players balance.</returns>
+    /// <exception cref="PersistenceExceptions">Throws this when <c>Build</c> is not possible.</exception>
     public Int32 Build(Int32 row, Int32 column, AreaType toBuild)
     {
-        if (_fields[row, column].GetAreaType() != "None") return 0;
-        if (NeighbouringFields(row, column).All(f => f.GetAreaType() != "Road")) return 0;
+        if (_fields[row, column].GetAreaType() != "None" || toBuild.GetAreaType() == "None") 
+            throw new PersistenceExceptions("Selected area is not empty or the input is the parent class!");
+        if (NeighbouringFields(row, column).All(f => f.GetAreaType() != "Road"))
+            throw new PersistenceExceptions("Selected area is not surrounded by a road.");
         _fields[row, column] = toBuild;
         return toBuild.BuildCost;
     }
 
+    /// <summary>
+    /// Set the <c>_fields</c> value at position <c>row, column</c> to <see cref="AreaType" /> (Parent class)
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    /// <returns>Money amount to be added to the players balanc</returns>
+    /// <exception cref="PersistenceExceptions">Throws this when <c>Remove</c> is not possible.</exception>
     public Int32 Remove(Int32 row, Int32 column)
     {
-        if (_fields[row, column].GetAreaType() == "None") return 0;
+        if (_fields[row, column].GetAreaType() == "None") 
+            throw new PersistenceExceptions("Selected area is already empty!");
         Int32 prize = _fields[row, column].RemovePrice;
         _fields[row, column] = new AreaType();
         return prize;
     }
 
     /// <summary>
-    /// Get all zone with the given AreaType
+    /// Get all zone with the given <see cref="AreaType" />
     /// </summary>
     /// <param name="areaType">Type of the area to search for.</param>
     /// <returns>A list containing x and y coordinates where the given Area occurs</returns>
@@ -96,10 +114,17 @@ public class Map
         return output;
     }
 
-    private List<AreaType> NeighbouringFields(Int32 row, Int32 col)
+    /// <summary>
+    /// Get the neighbouring fields in a <see cref="List{AreaType}" /> 
+    /// </summary>
+    /// <param name="row">Row of the examined field</param>
+    /// <param name="col">Column of the examined field</param>
+    /// <param name="area">[Optional] Size of area. Default value is 1.</param>
+    /// <returns>All fields <see cref="AreaType" /> around element at <c>row</c> and <c>col</c> in an <c>area</c></returns>
+    private List<AreaType> NeighbouringFields(Int32 row, Int32 col, Int32 area = 1)
     {
-        var output = from i in Enumerable.Range(row - 1, 3)
-                                       from j in Enumerable.Range(col - 1, 3)
+        var output = from i in Enumerable.Range(row - area, 1 + 2 * area)
+                                       from j in Enumerable.Range(col - area, 1 + 2 * area)
                                        where i >=  0 && i < RowSize && j >= 0 
                                              && j < ColumnSize && (i != row || j != col)
                                        select _fields[i, j];
