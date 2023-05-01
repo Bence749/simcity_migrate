73a07adc84e17548c1c304b7ae99c1fe931349bf
Bence Zsolt Beregi <dqk6te@INF.ELTE.HU>
2023-05-01 21:07:07 +0200

    SIM-72: Citizens fill up residential areas randomly -> Later happiness will determine which residential area fill up first

--- SimCity/Persistence/Map.cs
+++ SimCity/Persistence/Map.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Windows.Media;
+using System.Threading.Tasks;
 
 namespace SimCity.Persistence;
 
@@ -18,6 +18,10 @@ public class Map
     public Int32 RowSize => _fields.GetLength(0);
     public Int32 ColumnSize => _fields.GetLength(1);
     
+    public Int32 NumberOfCitizens => _fields.Cast<AreaType>().Select(y => y.NumberOfResidents).Sum();
+
+    public Int32 MaxCitizens => _fields.Cast<AreaType>().Select(y => (Int32) y.SizeOfZone).Sum();
+
     /// <summary>
     /// Constructor <c>Map</c> creating the desired map size and setting up the basic values
     /// </summary>
@@ -35,17 +39,6 @@ public class Map
         }
         else
             _fields[rows / 2 - 1, 0] = _fields[(rows / 2) + 1, 0] = new Road();
-    }
-
-    public Int32 MaxCitizens()
-    {
-        Int32 sumCit = 0;
-
-        foreach (var field in _fields)
-            if (field.GetAreaType() == "Residential")
-                sumCit += (int) field.SizeOfZone;
-
-        return sumCit;
     }
     
     public Int32 GetMaintenance()
@@ -72,8 +65,9 @@ public class Map
     {
         if (_fields[row, column].GetAreaType() != "None" || toBuild.GetAreaType() == "None") 
             throw new PersistenceExceptions("Selected area is not empty or the input is the parent class!");
-        if (NeighbouringFields(row, column).All(f => f.GetAreaType() != "Road"))
+        if (NeighbouringFields(row, column).All(f => f.Item1.GetAreaType() != "Road"))
             throw new PersistenceExceptions("Selected area is not surrounded by a road.");
+
         _fields[row, column] = toBuild;
         return toBuild.BuildCost;
     }
@@ -83,7 +77,7 @@ public class Map
     /// </summary>
     /// <param name="row"></param>
     /// <param name="column"></param>
-    /// <returns>Money amount to be added to the players balanc</returns>
+    /// <returns>Money amount to be added to the players balance</returns>
     /// <exception cref="PersistenceExceptions">Throws this when <c>Remove</c> is not possible.</exception>
     public Int32 Remove(Int32 row, Int32 column)
     {
@@ -99,37 +93,39 @@ public class Map
     /// </summary>
     /// <param name="areaType">Type of the area to search for.</param>
     /// <returns>A list containing x and y coordinates where the given Area occurs</returns>
-    public List<(Int32, Int32)> AvaibleZones(String areaType)
+    public List<(Int32, Int32)> AvailableZones(String areaType)
     {
-        List<(Int32, Int32)> output = new List<(int, int)>();
-
-        var indexedFields = _fields.Cast<AreaType>()
+        List<(Int32, Int32)> output = _fields.Cast<AreaType>()
             .Select((value, index) => 
                 new { Index = (index / _fields.GetLength(0), index % _fields.GetLength(1)),
-                    Value = value });
-        foreach (var area in indexedFields)
-            if (area.Value.GetAreaType() == areaType)
-                    output.Add(area.Index);
-
+                    Value = value })
+            .Where(y => y.Value.GetAreaType() == areaType)
+            .Select(y => y.Index).ToList();
+        
         return output;
     }
 
     /// <summary>
-    /// Get the neighbouring fields in a <see cref="List{AreaType}" /> 
+    /// Returns a list of neighbouring fields within the specified area around the given row and column indices.
     /// </summary>
-    /// <param name="row">Row of the examined field</param>
-    /// <param name="col">Column of the examined field</param>
+    /// <param name="row">Row of the center field</param>
+    /// <param name="col">Column of the center field</param>
     /// <param name="area">[Optional] Size of area. Default value is 1.</param>
-    /// <returns>All fields <see cref="AreaType" /> around element at <c>row</c> and <c>col</c> in an <c>area</c></returns>
-    private List<AreaType> NeighbouringFields(Int32 row, Int32 col, Int32 area = 1)
+    /// <returns>All fields <see cref="AreaType" /> and according indices around element at <c>row</c> and <c>col</c> in an <c>area</c></returns>
+    /// <remarks>
+    /// This method searches for neighbouring fields within a square area of size (2 * area + 1) x (2 * area + 1) centered around the specified row and column indices.
+    /// The method returns a list of tuples, where each tuple contains the field value and its row and column indices.
+    /// </remarks>
+    private List<(AreaType, (Int32, Int32))> NeighbouringFields(Int32 row, Int32 col, Int32 area = 1)
     {
-        var output = from i in Enumerable.Range(row - area, 1 + 2 * area)
+        var output = (from i in Enumerable.Range(row - area, 1 + 2 * area)
                                        from j in Enumerable.Range(col - area, 1 + 2 * area)
                                        where i >=  0 && i < RowSize && j >= 0 
                                              && j < ColumnSize && (i != row || j != col)
-                                       select _fields[i, j];
+                                       select (_fields[i, j], (i, j))).ToList();
 
-        return output.ToList();
+        return output;
 
     }
+
 }
