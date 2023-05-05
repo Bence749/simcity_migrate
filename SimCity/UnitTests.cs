aa79bf5950d1ec9a87202913b509c1d793e493a9
Bendeguz <bendeguz.werner@gmail.com>
2023-05-05 10:28:09 +0200

    SIM-107/feat: create CI/CD and empty test file

---
+++ SimCity/UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SimCity
+{
+
+}
+
+
+
+namespace HelloWorldAppTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Pass();
+        }
+    }
+}
